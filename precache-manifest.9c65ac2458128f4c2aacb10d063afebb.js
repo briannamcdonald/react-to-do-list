264546ef4a30d31cb3ebc6a29705b1f44666d044
Brianna McDonald <brmcdonald@mun.ca>
2020-08-19 15:39:16 -0230

    Updates

---
+++ precache-manifest.9c65ac2458128f4c2aacb10d063afebb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c383bb134268fb14346483c00e05f220",
+    "url": "/react-to-do-list/index.html"
+  },
+  {
+    "revision": "b172596ab2cf7b3d50bf",
+    "url": "/react-to-do-list/static/css/main.17b8de8d.chunk.css"
+  },
+  {
+    "revision": "da3276e04b9b9e47c46b",
+    "url": "/react-to-do-list/static/js/2.c112c8a6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-to-do-list/static/js/2.c112c8a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b172596ab2cf7b3d50bf",
+    "url": "/react-to-do-list/static/js/main.4e21f95f.chunk.js"
+  },
+  {
+    "revision": "9a7311025e51a361b9b1",
+    "url": "/react-to-do-list/static/js/runtime-main.5cb2f8e5.js"
+  },
+  {
+    "revision": "d0954e17802ece6fa552b97be5a421c7",
+    "url": "/react-to-do-list/static/media/background.d0954e17.jpg"
+  },
+  {
+    "revision": "a570f352dfa3d2487426fcc51d9beb19",
+    "url": "/react-to-do-list/static/media/background2.a570f352.jpg"
+  }
+]);
